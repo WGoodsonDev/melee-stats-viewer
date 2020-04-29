70bc1641e49e2edd64256cc090071482df86f29c
whgoodson <warren.goodson1@gmail.com>
2020-04-28 20:39:51 -0700

    Tolerable colors

--- src/containers/SidebarContainer/OpenButton/OpenButton.js
+++ src/containers/SidebarContainer/OpenButton/OpenButton.js
@@ -9,8 +9,8 @@ const openButton = (props) => {
 
     return (
         <div className={styles.OpenButton}>
-            <svg>
-                <circle cx={"10"} cy={"10"} r={"8"} stroke={"black"} strokeWidth={"2"} fill={"none"}></circle>
+            <svg height={"20px"} width={"20px"}>
+                <circle cx={"50%"} cy={"50%"} r={"8"} stroke={"black"} strokeWidth={"2"} fill={"none"}></circle>
             </svg>
             {props.children}
         </div>
